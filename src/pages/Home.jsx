82b6e410ec8cebd8ec15f97035e3774f10809403
Luis Sanchez <luisanchezdh@gmail.com>
2025-12-16 19:18:43 -0500

    basic homepage and structureed movie cards

---
+++ src/pages/Home.jsx
@@ -0,0 +1,54 @@
+import MovieCard from "../components/MovieCard";
+import { useState } from "react";
+
+const Home = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const movies = [
+    {
+      id: 1,
+      title: "John Wick",
+      release_date: "1999",
+    },
+    {
+      id: 2,
+      title: "Terminator",
+      release_date: "2000",
+    },
+    {
+      id: 3,
+      title: "The Matrix",
+      release_date: "2003",
+    },
+  ];
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    alert(searchQuery);
+    setSearchQuery("");
+  };
+
+  return (
+    <div className="home">
+      <form onSubmit={handleSearch} className="search-form">
+        <input
+          type="text"
+          placeholder="Search for movie..."
+          className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <button type="submit" className="search-button">
+          Search
+        </button>
+      </form>
+      <div className="movies-grid">
+        {movies.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
