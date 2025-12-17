5bcabd2ae50ad5b3020cf02443386a237fb713de
Luis Sanchez <luisanchezdh@gmail.com>
2025-12-16 19:37:35 -0500

    added basic navbar with routed paths to each page in movie app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import "./App.css";
+import Favorites from "./pages/Favorites";
 import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <>
-      <Home />;
+      <Navbar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </main>
     </>
   );
 };
